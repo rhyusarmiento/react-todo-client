739e6f4507e5f357f96fc28f615b9dae96e48f41
Rhyu Sarmiento <rhyu.sar.15@gmail.com>
2021-02-26 15:58:47 -0700

    add form for imput for title and have handlers

--- src/App.js
+++ src/App.js
@@ -4,13 +4,39 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      title: "",
+      done: false,
+      todos: []
     }
   }
+
+  handleChange = (e) => {
+    this.setState({
+      title: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("haah");
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <h1>Todo List</h1>
+        <form onSubmit={this.handleSubmit} className="add-todo">
+          <input
+            type="text"
+            placeholder="Add Todo"
+            value={this.state.title}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Add</button>
+        </form>
+        <div>
+
+        </div>
       </div>
     );
   }
