cdc81d8b6bdca9b4195212fd7d7c7523471dffd5
Rhyu Sarmiento <rhyu.sar.15@gmail.com>
2021-02-26 15:45:41 -0700

    Added to read me and made class component for App

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Todo react client
+
+## components:
+ - todo-form
+    - form 
+        - input
+        - button
+
+- todo-item
+    - checkbox
+    - title
+    - font awsome icon
+
+## State
+- title/todo?
+- done
+- todos
--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
-}
+import { Component } from "react";
 
-export default App;
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
